9e93f4fbab0a93da4de80b9212d8a715f413b704
ecoknows <jerico.villaraza19@gmail.com>
2021-04-16 20:25:55 +0800

    Deymsan

--- home/models.py
+++ home/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    ObjectList,
+    TabbedInterface,
+    
+)
 from wagtail.core.models import Page
 
 
@@ -10,14 +15,14 @@ class HomePage(Page):
 
 class Client(models.Model):
     client_no = models.CharField(max_length=255)
-    company_name = models.CharField(max_length=255)
+    company = models.ForeignKey('home.Companies',null=True,on_delete=models.CASCADE)
     address = models.CharField(max_length=255)
     contact_number = models.CharField(max_length=255)
     client_name = models.CharField(max_length=255)
 
     panels = [
         FieldPanel('client_no'),
-        FieldPanel('company_name'),
+        FieldPanel('company'),
         FieldPanel('address'),
         FieldPanel('contact_number'),
         FieldPanel('client_name'),
@@ -38,4 +43,33 @@ class Employee(models.Model):
         FieldPanel('position'),
     ]
 
+class Companies(models.Model):
+    company_name = models.CharField(max_length=255)
 
+    panels = [
+        FieldPanel('company_name'),
+    ]
+
+    def __str__(self):
+        return self.company_name
+
+class Question(models.Model):
+    company_name = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel('company_name'),
+    ]
+
+    def __str__(self):
+        return self.company_name
+
+
+
+class Assigns(models.Model):
+    client = models.ForeignKey('home.Client',null=True,on_delete=models.CASCADE)
+    employee = models.ForeignKey('home.Employee',null=True,on_delete=models.CASCADE)
+
+    panels = [
+        FieldPanel('client'),
+        FieldPanel('employee'),
+    ]
