00b0028c54f891a3ada446032ff74af091833bb2
Eco Villaraza <jerico.villaraza19@gmail.com>
2021-09-04 09:52:52 +0800

    Notification fix

--- performance_management_system/base/templatetags/notification_tags.py
+++ performance_management_system/base/templatetags/notification_tags.py
@@ -16,12 +16,15 @@ def for_evaluation_count(reciever):
     return len(evaluation)
 
 @register.simple_tag
-def get_latest_notif_date(user):
-    get_notifications = user.notifications.all()
+def get_latest_notif_date(user, notification_type):
+    get_notifications = user.notifications.filter(notification_type=notification_type)
 
     if len(get_notifications) <= 0:
         return None
-    
+
     get_latest_notif = get_notifications.latest('created_at')
+    
+    if get_latest_notif == None:
+        return None
 
     return get_latest_notif.created_at
